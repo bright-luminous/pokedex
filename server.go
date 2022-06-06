4bc054618fd68a4490310399b9f21d3cd508fbcd
bright-luminous <phisonkwan@gmail.com>
2022-06-06 12:35:59 +0700

    First commit

---
+++ server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/bright-luminous/pokedex/graph"
+	"github.com/bright-luminous/pokedex/graph/generated"
+	"github.com/go-chi/chi"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	r := chi.NewRouter()
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	r.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	r.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", defaultPort)
+	log.Fatal(http.ListenAndServe(":"+defaultPort, r))
+}
